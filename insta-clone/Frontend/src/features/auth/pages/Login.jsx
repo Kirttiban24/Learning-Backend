7f5514aafef7444e90036389c0e4f27498fb5a20
Kirttiban24 <153419795+Kirttiban24@users.noreply.github.com>
2026-06-30 22:50:31 +0530

    frontend

---
+++ insta-clone/Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../style/form.scss'
+
+const Login = () => {
+  return (
+    <main>
+        <div className="form-container">
+            <h1>Login</h1>
+            <form>
+                <input type="text" name='username'
+                placeholder='Enter Username' />
+                <input type="password" name='password'
+                placeholder='Enter Password' />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </main>
+  )
+}
+
+export default Login
