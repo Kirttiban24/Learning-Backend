049dff06a923d9f8138477e2c051520da4e78a13
Kirttiban24 <153419795+Kirttiban24@users.noreply.github.com>
2026-07-02 12:32:31 +0530

    update login and register

--- insta-clone/Backend/src/app.js
+++ insta-clone/Backend/src/app.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const cookies = require("cookie-parser")
-
+const cors = require("cors")
 
 const app = express()
-
 app.use(express.json())
 app.use(cookies())
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:5173"
+}))
 
 /* require routes */
 const authRouter = require("./router/auth.routes")
